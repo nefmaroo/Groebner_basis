911bfc8555e571cc3e4d02d1b4fe7580a2aeefd7
nefmaroo <manefedova_1@edu.hse.ru>
2020-02-12 12:57:20 +0300

    add methods to the template class

--- term.h
+++ term.h
@@ -6,11 +6,72 @@
 template<class TFieldType>
 class Term {
 public:
-
+    TFieldType getCoeffitient() const;
+    void setCoeffitient(TFieldType coeff);
+    Term<TFieldType>& operator*=(const Term<TFieldType>& other);
+    friend Term<TFieldType> operator*(const Term<TFieldType>& lhv, const Term<TFieldType>& rhv);
+    Term<TFieldType>& operator/=(const Term<TFieldType>& other);
+    friend Term<TFieldType> operator/(const Term<TFieldType>& lhv, const Term<TFieldType>& rhv);
+    friend bool operator==(const Term<TFieldType>& lhv, const Term<TFieldType>& rhv);
+    friend bool operator!=(const Term<TFieldType>& lhv, const Term<TFieldType>& rhv);
 private:
     TFieldType coefficient_;
     Monomial monomial_;
 };
 
 #endif //UNTITLED27_TERM_H
+
+template<class TFieldType>
+TFieldType Term<TFieldType>::getCoeffitient() const {
+    return coefficient_;
+}
+
+template<class TFieldType>
+void Term<TFieldType>::setCoeffitient(TFieldType coeff) {
+    coefficient_ = coeff;
+}
+
+template<class TFieldType>
+Term<TFieldType>& Term<TFieldType>::operator*=(const Term<TFieldType>& other) {
+    coefficient_ *= other.coefficient_;
+    monomial_ *= other.monomial_;
+    return *this;
+}
+
+template<class TFieldType>
+Term<TFieldType> operator*(const Term<TFieldType>& lhv, const Term<TFieldType>& rhv) {
+    Term<TFieldType> newTerm = lhv;
+    newTerm *= rhv;
+    return newTerm;
+}
+
+template<class TFieldType>
+Term<TFieldType>& Term<TFieldType>::operator/=(const Term<TFieldType>& other) {
+    if (other.coefficient_ != 0) //?
+    {
+        coefficient_ /= other.coefficient_;
+        monomial_ *= other.monomial_;
+    }
+    return *this;
+}
+
+template<class TFieldType>
+Term<TFieldType> operator/(const Term<TFieldType>& lhv, const Term<TFieldType>& rhv) {
+    Term<TFieldType> newTerm = lhv;
+    newTerm /= rhv;
+    return newTerm;
+}
+
+template<class TFieldType>
+bool operator==(const Term<TFieldType>& lhv, const Term<TFieldType>& rhv) {
+    return (lhv.coefficient_ == rhv.coefficient_) && (lhv.monomial_ == rhv.monomial_);
+}
+
+template<class TFieldType>
+bool operator!=(const Term<TFieldType>& lhv, const Term<TFieldType>& rhv) {
+    return !(lhv == rhv);
+}
+
+
+
 
