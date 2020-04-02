aa4a572de457e58fb45737d8af386fa52adc4888
nefmaroo <manefedova_1@edu.hse.ru>
2020-04-02 22:29:31 +0300

    Creation of a monomial comparison class with methods

--- monomial_order.cpp
+++ monomial_order.cpp
@@ -1,4 +1,38 @@
-//
-// Created by macbook on 14.02.2020.
-//
+#include "monomial_order.h"
 
+bool MonomialOrder::Lex::cmp(const Monomial& first, const Monomial& second) {
+    if (first == second)
+        return false;
+    int nonEqualDegree = 0;
+    while (first.getDegree(nonEqualDegree) == second.getDegree(nonEqualDegree))
+        ++nonEqualDegree;
+    return first.getDegree(nonEqualDegree) < second.getDegree(nonEqualDegree);
+}
+
+
+bool MonomialOrder::Lex::operator()(const Monomial& first, const Monomial& second) const {
+    return cmp(first, second);
+}
+
+bool MonomialOrder::Deg::cmp(const Monomial& first, const Monomial& second) {
+    return first.getTotalDegree() < second.getTotalDegree();
+}
+
+
+bool MonomialOrder::Deg::operator()(const Monomial& first, const Monomial& second) const {
+    return cmp(first, second);
+}
+
+template <class T1, class T2>
+bool MonomialOrder::LexSum<T1, T2>::cmp(const Monomial& first, const Monomial& second){
+    if (T1::cmp(first, second))
+        return true;
+    if (T1::cmp(second, first))
+        return false;
+    return T2::cmp(first, second);
+}
+
+template <class T1, class T2>
+bool MonomialOrder::LexSum<T1, T2>::operator()(const Monomial& first, const Monomial& second) const {
+    return cmp(first, second);
+}
--- monomial_order.h
+++ monomial_order.h
@@ -1,8 +1,29 @@
-//
-// Created by macbook on 14.02.2020.
-//
-
 #ifndef GROEBNER_BASIS_MONOMIAL_ORDER_H
 #define GROEBNER_BASIS_MONOMIAL_ORDER_H
+#include "monomial.h"
+
+class MonomialOrder {
+    class Lex {
+
+    public:
+        static bool cmp(const Monomial& first, const Monomial& second);
+        bool operator()(const Monomial& first, const Monomial& second) const;
+    };
+
+
+    class Deg {
+
+    public:
+        static bool cmp(const Monomial& first, const Monomial& second);
+        bool operator()(const Monomial& first, const Monomial& second) const;
+    };
+
+    template <class T1, class T2>
+    class LexSum
+    {
+        static bool cmp(const Monomial& first, const Monomial& second);
+        bool operator()(const Monomial& first, const Monomial& second) const;
+    };
+};
 
 #endif //GROEBNER_BASIS_MONOMIAL_ORDER_H
