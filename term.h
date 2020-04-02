fc57ebc6fe5a3728197845a02b38e2be293063a3
nefmaroo <manefedova_1@edu.hse.ru>
2020-04-02 22:27:36 +0300

    Relocate friend operators, fix minor mistakes

--- term.h
+++ term.h
@@ -1,75 +1,62 @@
-#ifndef UNTITLED27_TERM_H
-#define UNTITLED27_TERM_H
+#pragma once
 
 #include "monomial.h"
+#include "./boost/rational.hpp"
 
 template<class TFieldType>
 class Term {
 public:
-    TFieldType getCoeffitient() const;
-    void setCoeffitient(TFieldType coeff);
-    Term<TFieldType>& operator*=(const Term<TFieldType>& other);
-    friend Term<TFieldType> operator*(const Term<TFieldType>& lhv, const Term<TFieldType>& rhv);
-    Term<TFieldType>& operator/=(const Term<TFieldType>& other);
-    friend Term<TFieldType> operator/(const Term<TFieldType>& lhv, const Term<TFieldType>& rhv);
-    friend bool operator==(const Term<TFieldType>& lhv, const Term<TFieldType>& rhv);
-    friend bool operator!=(const Term<TFieldType>& lhv, const Term<TFieldType>& rhv);
+    const TFieldType& getCoefficient() const;
+    void setCoefficient(TFieldType coeff);
+    Term& operator*=(const Term<TFieldType>& other);
+    friend Term operator*(const Term& lhv, const Term& rhv) {
+        Term newTerm = lhv;
+        newTerm *= rhv;
+        return newTerm;
+    }
+    Term& operator/=(const Term& other);
+    friend Term operator/(const Term& lhv, const Term& rhv) {
+        Term newTerm = lhv;
+        newTerm /= rhv;
+        return newTerm;
+    }
+    friend bool operator==(const Term<TFieldType>& lhv, const Term<TFieldType>& rhv) {
+        return (lhv.coefficient_ == rhv.coefficient_) && (lhv.monomial_ == rhv.monomial_);
+    }
+    friend bool operator!=(const Term<TFieldType>& lhv, const Term<TFieldType>& rhv) {
+        return !(lhv == rhv);
+    }
 private:
     TFieldType coefficient_;
     Monomial monomial_;
 };
 
-#endif //UNTITLED27_TERM_H
-
 template<class TFieldType>
-TFieldType Term<TFieldType>::getCoeffitient() const {
+const TFieldType& Term<TFieldType>::getCoefficient() const {
     return coefficient_;
 }
 
 template<class TFieldType>
-void Term<TFieldType>::setCoeffitient(TFieldType coeff) {
-    coefficient_ = coeff;
+void Term<TFieldType>::setCoefficient(TFieldType coeff) {
+    coefficient_ = std::move(coeff);
 }
 
 template<class TFieldType>
 Term<TFieldType>& Term<TFieldType>::operator*=(const Term<TFieldType>& other) {
     coefficient_ *= other.coefficient_;
     monomial_ *= other.monomial_;
+    if (other.coefficient_ == 0)
+
     return *this;
 }
 
-template<class TFieldType>
-Term<TFieldType> operator*(const Term<TFieldType>& lhv, const Term<TFieldType>& rhv) {
-    Term<TFieldType> newTerm = lhv;
-    newTerm *= rhv;
-    return newTerm;
-}
 
 template<class TFieldType>
 Term<TFieldType>& Term<TFieldType>::operator/=(const Term<TFieldType>& other) {
-    if (other.coefficient_ != 0) //?
-    {
-        coefficient_ /= other.coefficient_;
-        monomial_ *= other.monomial_;
-    }
+    assert(other.coefficient_ != 0 && "Division by zero is detected");
+    coefficient_ /= other.coefficient_;
+    monomial_ *= other.monomial_;
     return *this;
-}
-
-template<class TFieldType>
-Term<TFieldType> operator/(const Term<TFieldType>& lhv, const Term<TFieldType>& rhv) {
-    Term<TFieldType> newTerm = lhv;
-    newTerm /= rhv;
-    return newTerm;
-}
-
-template<class TFieldType>
-bool operator==(const Term<TFieldType>& lhv, const Term<TFieldType>& rhv) {
-    return (lhv.coefficient_ == rhv.coefficient_) && (lhv.monomial_ == rhv.monomial_);
-}
-
-template<class TFieldType>
-bool operator!=(const Term<TFieldType>& lhv, const Term<TFieldType>& rhv) {
-    return !(lhv == rhv);
 }
 
 
