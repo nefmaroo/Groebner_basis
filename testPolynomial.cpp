81dcd2e26b425bc4496e5bedf547165cc1c671de
nefmaroo <manefedova_1@edu.hse.ru>
2020-05-12 21:04:45 +0300

    Add polinomial comparison tests for different monomial order

--- testPolynomial.cpp
+++ testPolynomial.cpp
@@ -1,18 +1,21 @@
+#include <ios>
 #include "polynomial.h"
+#include "polynomial_order.h"
+#include "term_order.h"
 #include <boost/rational.hpp>
 using boost::rational;
 
 void testPolynomial() {
     namespace gb = Groebner;
-    using gb::Term, gb::Polynomial, gb::Deg;
+    using gb::Term, gb::Polynomial, gb::Lex, gb::Deg, gb::LexSum, gb::PolynomialOrder, gb::TermOrder;
     using Rational = rational<int>;
 
     // Input/Output test
     std::cout << "1) Test polynomial input and output:\n";
-    Polynomial<Rational> polynomial1({Term<Rational>(Rational(-2), {1, 2, 0}),
+    Polynomial<Rational> polynomial1({Term<Rational>(Rational(-2), {1, 20, 0}),
                                       Term<Rational>(Rational(1,2)), Term<Rational>(4, {6, 0, 3, 1})});
     std::cout << "polynomial1 is " << polynomial1 << "\n";
-    Polynomial<Rational> polynomial2({Term<Rational>(1, {6, 0, 3, 1}), Term<Rational>(-5, {9, 4, 8})});
+    Polynomial<Rational> polynomial2({Term<Rational>(1, {6, 0, 3, 1}), Term<Rational>(-5, {6, 4, 8})});
     std::cout << "polynomial2 is " << polynomial2 << "\n\n";
 
 
@@ -30,17 +33,37 @@ void testPolynomial() {
     std::cout << "term * polynomial:\n" << term * polynomial1 << "\n\n";
 
 
-    // Test comparison operations for polynomials
+    // Test comparison for equality for two polynomials
     std::cout << "4) Test comparison operations for polynomial1 and polynomial2\n";
     std::cout << "Is polynomial1 equals polynomial2?\n";
     std::cout << "polynomial1 is " << polynomial1 << "\n" << "polynomial2 is " << polynomial2 << "\n";
     if (polynomial1 != polynomial2)
         std::cout << "Answer: NO\n\n";
 
+    // Test comparison for different order
+    std::cout << "5) Comparison test of two polinomial for different monomial order:\n";
+    std::cout << "polynomial1 is " << polynomial1 << "\n";
+    std::cout << "polynomial2 is " << polynomial2 << "\n\n";
+    std::cout << "Lexicographic order: \n";
+    PolynomialOrder<Lex> orderTypeLex;
+    std::cout << std::boolalpha << "polynomial1 > polynomial2 is " << orderTypeLex(polynomial1, polynomial2) << "\n\n";
+    std::cout << "Total degree order: \n";
+    PolynomialOrder<Deg> orderTypeDeg;
+    std::cout << std::boolalpha << "polynomial1 > polynomial2 is " << orderTypeDeg(polynomial1, polynomial2) << "\n\n";
+    std::cout << "LexDeg order: \n";
+    PolynomialOrder<LexSum<Lex, Deg>> orderTypeLexDeg;
+    std::cout << std::boolalpha << "polynomial1 > polynomial2 is " << orderTypeLexDeg(polynomial1, polynomial2) << "\n\n";
+    std::cout << "DegLex order: \n";
+    PolynomialOrder<LexSum<Deg, Lex>> orderTypeDegLex;
+    std::cout << std::boolalpha << "polynomial1 > polynomial2 is " << orderTypeDegLex(polynomial1, polynomial2) << "\n\n";
+
+
     // Test for S-Polynomial calculation
-    std::cout << "5) Test for S-Polynomial calculation of polynomial1 and polynomial2\n";
+    std::cout << "6) Test for S-Polynomial calculation of polynomial1 and polynomial2\n";
     std::cout << "S-Polynomial of " << polynomial1 << "and " << polynomial2 << "is\n"
     << SPolynomial(polynomial1, polynomial2);
+
+
 
 }
 
